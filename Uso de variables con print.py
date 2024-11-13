c041f3fcabf0d601fa94436c3e493e797d351002
Leotorrero6 <leonardo.jose.torrero@gmail.com>
2024-11-13 10:39:59 -0500

    Add files via upload

--- Uso de variables con print.py
+++ Uso de variables con print.py
@@ -2,10 +2,10 @@ frase = "*** FRASE MOTIVADORA: ***"
 print(frase)
 
 
-uno = ["La vida no siempre es fácil, pero en cada desafío hay una oportunidad para crecer."]
-dos = ["En cada tropiezo, hay una lección que te acerca más a la persona que estás destinada a ser."]
-tres = ["No dejes que las dificultades te hagan dudar de ti mismo, porque cada vez que caes, tienes"]
-cuatro = ["la posibilidad de levantarte con más fuerza, más sabiduría y una determinación renovada. "]
+uno = "La vida no siempre es fácil, pero en cada desafío hay una oportunidad para crecer."
+dos = "En cada tropiezo, hay una lección que te acerca más a la persona que estás destinada a ser."
+tres = "No dejes que las dificultades te hagan dudar de ti mismo, porque cada vez que caes, tienes"
+cuatro = "la posibilidad de levantarte con más fuerza, más sabiduría y una determinación renovada. "
 
 print(uno)
 print(dos)
